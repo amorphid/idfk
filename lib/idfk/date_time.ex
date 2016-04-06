3a5c7baf99e425c7bf0648338c3562dedcd284dc
Michael Pope <mpope.cr@gmail.com>
2016-04-06 15:45:57 -0700

    Implement DateTime.gregorian_to_posix

--- lib/idfk/date_time.ex
+++ lib/idfk/date_time.ex
@@ -1,10 +1,21 @@
 defmodule Idfk.DateTime do
   @doc """
-  The date 1 Jan 1970 @ 12:00 a.m. in seconds.  Hard coded for performance reasons.
+  Converts an integer formatted date_time based on the Gregorian epoch of 1 Jan 0000 @ 12:00 a.m., and you need an integer formatted date_time based on the posix epoch of  1 Jan 1970 @ 12:00 a.m.
 
-    # same as running this command
-    iex> :calendar.datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
-    62167219200
+      # same as this scenario
+      iex> posix_epoch_in_seconds(63627199062)
+      1459979862
+  """
+  def gregorian_to_posix(seconds) when is_integer(seconds) do
+    seconds - posix_epoch_in_seconds
+  end
+
+  @doc """
+  The datetime 1 Jan 1970 @ 12:00 a.m. in seconds.  Hard coded for performance reasons.
+
+      # same as this scenario
+      iex> :calendar.datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
+      62167219200
   """
   def posix_epoch_in_seconds do
     62167219200
--- spec/idfk/date_time_spec.exs
+++ spec/idfk/date_time_spec.exs
@@ -1,12 +1,28 @@
 defmodule Idfk.DateTimeSpec do
   use ESpec
 
+  let :posix_epoch_in_seconds, do: 62167219200
+
   subject do: Idfk.DateTime
+
+  context "#gregorian_to_posix" do
+    it "returns an integer" do
+      gregorian = posix_epoch_in_seconds + 10
+      result    = subject.gregorian_to_posix(gregorian)
+      expect result |> to(eq 10)
+    end
+
+    it "raise error w/ float input" do
+      gregorian = posix_epoch_in_seconds + 10.12345
+      result    = fn -> subject.gregorian_to_posix(gregorian) end
+      expect result |> to(raise_exception)
+    end
+  end
 
   context "#posix_epoch_in_seconds" do
     it "equals 62167219200" do
       result = subject.posix_epoch_in_seconds
-      expect result |> to(eq 62167219200)
+      expect result |> to(eq posix_epoch_in_seconds)
     end
   end
 end
