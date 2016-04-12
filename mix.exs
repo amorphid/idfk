4654c61f3dae3442cefa78faec5a1fb48ab727ba
Michael Pope <mpope.cr@gmail.com>
2016-04-12 16:10:31 -0700

    Update mix.exs w/ a few publishing details

--- mix.exs
+++ mix.exs
@@ -1,17 +1,21 @@
 defmodule Idfk.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :idfk,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
-  end
-
   def application do
     [applications: [:logger]]
+  end
+
+  def project do
+    [
+      app:              :idfk,
+      build_embedded:   Mix.env == :prod,
+      description:      description,
+      deps:             deps,
+      elixir:           "~> 1.2",
+      package:          package,
+      start_permanent:  Mix.env == :prod,
+      version:          "0.0.1"
+    ]
   end
 
   defp deps do
@@ -20,6 +24,20 @@ defmodule Idfk.Mixfile do
       {:espec, "~> 0.8.17", only: :test},
       {:ex_doc, "~> 0.11", only: :dev},
       {:timex, "~> 2.1.4"}
+    ]
+  end
+
+  defp description do
+    """
+    The library of Elixir chunks of code that didn't clearly belong anywhere else.
+    """
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/amorphid/idfk"},
+      maintainers: ["Michael Pope"]
     ]
   end
 end
